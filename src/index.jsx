3c64f4f284e5db8d86b7ace018133ed4fe921d7e
El Alami Idriss <elalami.idriss.7391@gmail.com>
2023-02-23 13:19:55 +0100

    OK

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import './utils/i18n';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-258174985-1');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
